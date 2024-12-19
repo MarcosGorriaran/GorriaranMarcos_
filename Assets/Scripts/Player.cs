d0f95ad32e5ec452d7c0b92b680b2ca56532c70a
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-12-19 16:46:48 +0100

    Implementing InputController for player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : Entity
+public class Player : Entity, PlayerController.IPlayerActions
 {
+    PlayerController inputController;
+    protected override void Awake()
+    {
+        base.Awake();
+        inputController = new PlayerController();
+        inputController.Player.SetCallbacks(this);
+    }
+    public void OnAiming(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnShooting(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
